3843ef4b7e5f03c74354c809f70ae098e8806dbe
PSurya9195 <38082208+PSurya9195@users.noreply.github.com>
2020-12-10 09:59:53 +0530

    models information

--- EBA/models.py
+++ EBA/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+#Here in django, tables are called as models
+#No need of seperate SQL Developer to design DB schema
 
+
+#This is the table for customer details
 class Customer(models.Model):
   GENDER_CHOICES = [
     ('M', 'Male'),
@@ -18,6 +22,8 @@ class Customer(models.Model):
     return self.user.username
 
 
+
+#Location restaurant model is for to query restaurants based on searched location, and if no location matched , no object will be returned
 class Location_Restaurant(models.Model):
   Location = models.CharField(max_length=50)
   Restaurant_Name = models.CharField(max_length=50)
@@ -27,6 +33,9 @@ class Location_Restaurant(models.Model):
   def __str__(self):
     return self.Location + self.Restaurant_Name
 
+ 
+
+#Restaurant Food model is for showing food of particular restaurant after querying up through location using forms
 class Restaurant_Food_New(models.Model):
   Restaurant_Id = models.ForeignKey(Location_Restaurant, on_delete=models.CASCADE)
   Food_Name = models.CharField(max_length=50)
